30116ec6058d57f07bcb5b3113eb15c92c7a5328
lileeyao@gmail.com <lileeyao@github.com/lileeyao/leet>
2014-03-04 23:05:20 -0700

    py

---
+++ random_num.py
@@ -0,0 +1,21 @@
+import random
+
+def random_subset( it, K ):
+    result = []
+    N = 0
+    s = 0
+
+    for item in it:
+        N += 1
+        if len( result ) < K:
+            result.append( item )
+        else:
+            s = int(random.random() * N)
+        if s < K:
+            result[ s ] = item
+
+    return result
+lst = range(100)
+it = range(1,100,1)
+for i in it:
+    print random_subset(it, 1)
