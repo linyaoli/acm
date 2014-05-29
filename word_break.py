377740cc7a4037fc4e66d54f86ab8e0480cd482e
lileeyao@gmail.com <lileeyao@github.com/lileeyao/leet>
2014-05-29 10:53:29 -0700

    2014-5-28

--- word_break.py
+++ word_break.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python
 class Solution:
     def wordBreak(self, s, dict):
-        _s = s
-        while s != "":
-            for i in range(1, len(s), 1):
-                if s[0:i] in dict:
-                    s = s[i:]
-            if s == "":
-                return True
-            if s == _s:
-                return False
-            if s != _s:
-                s = _s
-        return True
+        if len(dict) == 0:
+            return False
+        s = '#' + s
+        _len = len(s)
+        p = [False] * _len
+        p[0] = True
+        for i in range(1, _len, 1):
+            for j in range(0, i, 1):
+                if s[j + 1:i + 1] in dict:
+                    p[i] = p[j]
+                else:
+                    p[i] = False
+                if p[i]:
+                    break
+        print s
+        print dict
+        print p
+        return p[_len - 1]
+
+sol = Solution()
+#s = "abcdefg"
+#dict = ["a", "bc", "bcd", "ef", "efg"]
+s = "a"
+dict = ["b"]
+print sol.wordBreak(s, dict)
