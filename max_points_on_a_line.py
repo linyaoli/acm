c54ee0f5aa4ac02a6434051684ad68b7b270a2d1
lileeyao@gmail.com <lileeyao@github.com/lileeyao/leet>
2014-11-04 15:33:31 -0700

    2014-11-04

--- max_points_on_a_line.py
+++ max_points_on_a_line.py
@@ -1,25 +1,51 @@
+# Definition for a point
+class Point:
+     def __init__(self, a=0, b=0):
+         self.x = a
+         self.y = b
+
+# TODO DON'T MESS UP WITH INT AND FLOAT !
+#      FIND THESE TIPS IN LINE 23 ~ 26
 class Solution:
     def maxPoints(self, points):
         max_count = 0
-        count = 0
-        radient = 0
-        next_line = True
-        for point in points:
-            for _point in points:
-                if next_line is True:
-                    radient = (_point.y - point.y) / (_point.x - point.x)
-                    next_line = False
-                if point.x == _point.x and point.y == _point.y:
+        for idx1 in xrange(len(points)):
+            dup = 1
+            radient_set = {0:0}
+            for idx2 in xrange(len(points)):
+                if idx1 == idx2:
                     continue
-                if next_line is False:
-                    if radient == (_point.y - point.y) / (_point.x - point.x):
-                        count = count + 1
-            if count > max_count:
-                max_count = count
-            next_line = True
+                if points[idx1].x == points[idx2].x and points[idx1].y == points[idx2].y:
+                    #count duplicates
+                    dup += 1
+                    continue
+                k = 0.0
+                if points[idx1].x - points[idx2].x == 0:
+                    k = 999999999.0 # indicate k = Inf
+                else:
+                    k = (points[idx1].y - points[idx2].y) * 1.0 / (points[idx1].x - points[idx2].x)
+
+                if k not in radient_set:
+                    radient_set[k] = 1
+                else:
+                    radient_set[k] += 1
+
+            for kys in radient_set.keys():
+                if radient_set[kys] + dup > max_count:
+                    max_count = radient_set[kys] + dup
+
+        return max_count
+
 
 sol = Solution()
-sol.maxPoints()
+pts1 = Point(84, 250)
+pts2 = Point(0, 0)
+pts3 = Point(1, 0)
+pts4 = Point(0, -70)
+pts5 = Point(0, -70)
+pts6 = Point(1, -1)
+pts7 = Point(21, 10)
+pts8 = Point(42, 90)
+pts9 = Point(-42, -230)
 
-
-
+print sol.maxPoints([pts1, pts2,pts3,pts4,pts5,pts6,pts7,pts8,pts9])
--- sort_list.py
+++ sort_list.py
@@ -1,43 +1,65 @@
-#!/usr/bin/python
-
-
+# Definition for singly-linked list.
 class ListNode:
-    def _init_(self, x):
+    def __init__(self, x):
         self.val = x
         self.next = None
 
-
 class Solution:
+    # @param head, a ListNode
+    # @return a ListNode
     def sortList(self, head):
+        #!/usr/bin/python
         if head is None:
             return None
+        if head.next is None:
+            return head
+
         length = 0
         it = head
-        while it.next is not None:
+        while it is not None:
             length = length + 1
             it = it.next
 
-        if length == 1:
-            return head
         return self.merge_sort(head, length)
 
     def merge_sort(self, head, length):
+        if length == 1:
+            tmp = head
+            head = head.next
+            tmp.next = None
+            return tmp
 
         leftHead = self.merge_sort(head, length / 2)
         rightHead = self.merge_sort(head, length - length / 2)
+
         return self.merge(leftHead, rightHead)
 
     def merge(self, first, second):
-        head = ListNode()
+        head = ListNode(0)
         head_ori = head
-        while first is not None and second is not None:
-            fv = first.val
-            sv = second.val
+        while first is not None or second is not None:
+            fv = sv = 99999999
+            if first is not None and second is None:
+                fv = first.val
+            elif first is None and second is not None:
+                sv = second.val
+            elif first is not None and second is not None:
+                fv = first.val
+                sv = second.val
+
             if fv <= sv:
                 head.next = first
                 first = first.next
             else:
                 head.next = second
                 second = second.next
-            head = head_ori.next
-            return head
+            head = head.next
+
+        return head_ori.next
+
+sol = Solution()
+node1 = ListNode(2)
+node2 = ListNode(1)
+node1.next = node2
+node2.next = None
+sol.sortList(node1)
