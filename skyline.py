4178767af0e6649e3b72fdbfee7f124f65307c82
lileeyao@gmail.com <lileeyao@github.com/lileeyao/leet>
2015-05-04 21:39:29 -0700

    Mon May  4 21:39:29 MST 2015

---
+++ skyline.py
@@ -0,0 +1,26 @@
+"""
+You are given a set of n rectangles in no particular order.
+They have varying widths and heights, but their bottom edges
+are collinear, so that they look like buildings on a skyline.
+For each rectangle, you’re given the x position of the left edge,
+the x position of the right edge, and the height. Your task is
+to draw an outline around the set of rectangles so that you can
+see what the skyline would look like when silhouetted at night.
+
+e.g.
+
+input: (left, right, height)
+
+[
+    (0, 1, 2),
+    (2, 4, 5),
+    (3, 5, 3),
+    (5, 7, 2)
+]
+
+output:
+
+
+
+
+"""
