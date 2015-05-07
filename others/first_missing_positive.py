7a438fa678c2b29baba31969a95e26c6da3c6a41
lileeyao@gmail.com <lileeyao@github.com/lileeyao/>
2015-05-07 16:02:34 -0700

    Thu May  7 16:02:34 MST 2015

--- others/first_missing_positive.py
+++ others/first_missing_positive.py
@@ -1,9 +1,15 @@
+"""
+brute: sort, O(nlogn)
+op: O(n)
+
+"""
 class Solution:
     # @param A, a list of integers
     # @return an integer
     def firstMissingPositive(self, A):
         n = len(A)
         for i in xrange(n):
+            print '---'
             while A[i] != i + 1:
                 if A[i] <= 0 or A[i] > n or A[i] == A[A[i] -1]:
                     break
@@ -11,6 +17,8 @@ class Solution:
                 b = A[i] - 1
                 # put the num to A[num]
                 A[a], A[b] = A[b], A[a]
+                print A
+            #print A, A[i], i + 1
 
         for i in xrange(n):
             if A[i] != i + 1:
@@ -19,4 +27,4 @@ class Solution:
         return n + 1
 
 sol = Solution()
-print sol.firstMissingPositive([2,1])
+print sol.firstMissingPositive([0,4,4,2,1,6])
