dc72b2a8954e4d862149c8a76261f2bec08ad2d6
Linyao Li <lileeyao@outlook.com>
2018-03-15 11:23:15 +0800

    greedy greedy, I'm not that good at greedy

--- various/ruby/assign_cookies.rb
+++ various/ruby/assign_cookies.rb
@@ -3,5 +3,20 @@
 # @param {Integer[]} s
 # @return {Integer}
 def find_content_children(g, s)
-    
+  # Stop using god damn single-letter variables !
+  children = g.sort
+  cookies = s.sort
+
+  cookie_index = 0
+  child_index = 0
+
+  while cookie_index < cookies.size && child_index < children.size
+    child_index += 1 if children[child_index] <= cookies[cookie_index]
+    cookie_index += 1
+  end
+
+  child_index
 end
+
+p find_content_children([1,2,3], [1,1])
+p find_content_children([1,2], [1,2,3])
