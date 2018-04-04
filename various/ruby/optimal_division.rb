f5d008d746cf019fe9a79605a89a170a02b07363
Linyao Li <lileeyao@outlook.com>
2018-04-04 16:43:47 +0800

    refactor

--- various/ruby/optimal_division.rb
+++
@@ -1,6 +0,0 @@
-# https://leetcode.com/problems/optimal-division/description/
-# @param {Integer[]} nums
-# @return {String}
-def optimal_division(nums)
-  # TODO 
-end
--- various/ruby/patching_array.rb
+++
@@ -1,7 +0,0 @@
-# @param {Integer[]} nums
-# @param {Integer} n
-# @return {Integer}
-# https://leetcode.com/problems/patching-array/#/description
-def min_patches(nums, n)
-
-end
