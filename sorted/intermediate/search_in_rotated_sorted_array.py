6e66a299ed1f3db173445fa18d4ea9a123a907fb
Linyao Li <lileeyao@outlook.com>
2018-04-28 14:38:33 +0800

    hah!

--- sorted/intermediate/search_in_rotated_sorted_array.py
+++ sorted/intermediate/search_in_rotated_sorted_array.py
@@ -27,42 +27,5 @@ class Solution:
 
 sol = Solution()
 print sol.search([5, 1, 3], 3)
+print sol.search([1, 3,4,5,6,7,8,9], 5)
 
-
-class entryExit(object):
-    def __init__(self, f):
-        self.f = f
-    def __call__(self):
-        print "Entering", self.f.__name__
-        self.f()
-        print "Exited", self.f.__name__
-
-@entryExit
-def func1():
-    print "inside func1"
-@entryExit
-def func2():
-    print "inside func2"
-
-func1()
-func2()
-print "---------------"
-def entryExit(f):
-    def new_f():
-        print "Entering", f.__name__
-        f()
-        print "Exited", f.__name__
-    return new_f
-
-@entryExit
-def func1():
-    print "inside func1()"
-
-@entryExit
-def func2():
-    print "inside func2()"
-
-func1()
-func2()
-print func1.__name__
-print func2.__name__
---
+++ various/rust/salt_is_my_sugar/target/debug/salt_is_my_sugar.d
@@ -0,0 +1,1 @@
+/Users/linyaoli/Repo/acm/various/rust/salt_is_my_sugar/target/debug/salt_is_my_sugar.dSYM: /Users/linyaoli/Repo/acm/various/rust/salt_is_my_sugar/src/main.rs
