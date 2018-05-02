56514e823bb8d03060ed8dc030937790100db23f
Linyao Li <lileeyao@outlook.com>
2018-05-03 01:01:42 +0800

    remove rust

--- various/rust/salt_is_my_sugar/target/debug/deps/salt_is_my_sugar-f9fe27827bf9342f.d
+++
@@ -1,5 +0,0 @@
-/Users/linyaoli/Repo/acm/various/rust/salt_is_my_sugar/target/debug/deps/salt_is_my_sugar-f9fe27827bf9342f: src/main.rs
-
-/Users/linyaoli/Repo/acm/various/rust/salt_is_my_sugar/target/debug/deps/salt_is_my_sugar-f9fe27827bf9342f.d: src/main.rs
-
-src/main.rs:
